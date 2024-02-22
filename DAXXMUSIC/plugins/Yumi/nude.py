5fba60427e0fec76a7b98d57d90ef9a0f43562e7
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-02-22 18:35:47 +0530

    Create nude.py

---
+++ DAXXMUSIC/plugins/Yumi/nude.py
@@ -0,0 +1,20 @@
+import requests
+from pyrogram import filters
+from pyrogram.types import Message,InlineKeyboardButton,InlineKeyboardMarkup
+from pyrogram.enums import *
+from AvishaRobot import app
+
+
+@app.on_message(filters.command("nude"))
+async def nudes(_,message):
+    if message.chat.type != ChatType.PRIVATE:
+        return await message.reply_text("❍ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ᴜsᴀʙʟᴇ ɪɴ ᴘᴍ ғᴏʀ ɢʀᴏᴜᴘ ᴘʀᴏᴛᴇᴄᴛɪᴏɴ.",
+         reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("ɢᴏ ᴘᴍ", url=f"https://t.me/{app.me.username}?start=True")]
+            ]
+        ))
+    x = requests.get('https://api.night-api.com/images/nsfw',headers={"authorization": "pUieNWJRIs-2Q073qw9dddUcM3Vncmn-eusGidDCIw"})
+    await message.reply_photo(x.json()["content"]["url"])
+
+
