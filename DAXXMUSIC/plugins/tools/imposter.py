d337a9ef14fa7c003ab9ad52589e4ef4a0375d7f
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-02-22 18:23:34 +0530

    Update imposter.py

--- DAXXMUSIC/plugins/tools/imposter.py
+++ DAXXMUSIC/plugins/tools/imposter.py
@@ -1,9 +1,39 @@
+import random 
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from DAXXMUSIC.plugins.tools.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
 from DAXXMUSIC import app
 
+MISHI = [
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+]
 
+
+ROY = [
+    [
+        InlineKeyboardButton(
+            text="ᴀᴅᴅ ᴍᴇ",
+            url=f"https://t.me/sinewyxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/SiNeWyuPt")
+    ],
+]
 
 
 @app.on_message(filters.group & ~filters.bot & ~filters.via_bot, group=69)
@@ -25,11 +55,10 @@ async def chk_usr(_, message: Message):
         or lastname_before != message.from_user.last_name
     ):
         msg += f"""
-**Ⰶ ᴜsᴇʀ sʜᴏʀᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ Ⰶ**
+**♥︎ ᴜsᴇʀ sʜᴏʀᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ♥︎**
 
 **๏ ɴᴀᴍᴇ** ➛ {message.from_user.mention}
 **๏ ᴜsᴇʀ ɪᴅ** ➛ {message.from_user.id}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
 """
     if usernamebefore != message.from_user.username:
         usernamebefore = f"@{usernamebefore}" if usernamebefore else "NO USERNAME"
@@ -39,11 +68,10 @@ async def chk_usr(_, message: Message):
             else "NO USERNAME"
         )
         msg += """
-**Ⰶ ᴄʜᴀɴɢᴇᴅ ᴜsᴇʀɴᴀᴍᴇ Ⰶ**
+**♥︎ ᴄʜᴀɴɢᴇᴅ ᴜsᴇʀɴᴀᴍᴇ ♥︎**
 
-**๏ ᴡɪᴛʜᴏᴜᴛ ᴄʜᴀɴɢᴇ ᴜsᴇʀɴᴀᴍᴇ** ➛ {bef}
-**๏ ᴀғᴛᴇʀ ᴄʜᴀɴɢᴇ ᴜsᴇʀɴᴀᴍᴇ** ➛ {aft}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
 """.format(bef=usernamebefore, aft=usernameafter)
         await add_userdata(
             message.from_user.id,
@@ -53,11 +81,10 @@ async def chk_usr(_, message: Message):
         )
     if first_name != message.from_user.first_name:
         msg += """
-**Ⰶ ᴄʜᴀɴɢᴇs ғɪʀsᴛ ɴᴀᴍᴇ Ⰶ**
+**♥︎ ᴄʜᴀɴɢᴇs ғɪʀsᴛ ɴᴀᴍᴇ ♥︎**
 
-**๏ ᴡɪᴛʜᴏᴜᴛ ᴄʜᴀɴɢᴇ ғʀɪsᴛ ɴᴀᴍᴇ** ➛ {bef}
-**๏ ᴀғᴛᴇʀ ᴄʜᴀɴɢᴇ ғʀɪsᴛ ɴsᴍᴇ** ➛ {aft}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
 """.format(
             bef=first_name, aft=message.from_user.first_name
         )
@@ -71,11 +98,10 @@ async def chk_usr(_, message: Message):
         lastname_before = lastname_before or "NO LAST NAME"
         lastname_after = message.from_user.last_name or "NO LAST NAME"
         msg += """
-**Ⰶ ᴄʜᴀɴɢᴇs ʟᴀsᴛ ɴᴀᴍᴇ Ⰶ**
+**♥︎ ᴄʜᴀɴɢᴇs ʟᴀsᴛ ɴᴀᴍᴇ ♥︎**
 
-**๏ ᴡɪᴛʜᴏᴜᴛ ᴄʜᴀɴɢᴇ ʟᴀsᴛ ɴᴀᴍᴇ** ➛ {bef}
-**๏ ᴀғᴛᴇʀ ᴄʜᴀɴɢᴇ ʟᴀsᴛ ɴᴀᴍᴇ** ➛ {aft}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
 """.format(
             bef=lastname_before, aft=lastname_after
         )
@@ -86,7 +112,7 @@ async def chk_usr(_, message: Message):
             message.from_user.last_name,
         )
     if msg != "":
-        await message.reply_photo("https://telegra.ph/file/ded86839c47bdd843109b.jpg", caption=msg)
+        await message.reply_photo(random.choice(MISHI), caption=msg, reply_markup=InlineKeyboardMarkup(ROY),)
 
 
 @app.on_message(filters.group & filters.command("imposter") & ~filters.bot & ~filters.via_bot)
