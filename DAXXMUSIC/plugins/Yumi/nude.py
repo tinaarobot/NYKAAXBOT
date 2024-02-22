89185d31f40753f41db57c22a4507e51219b5654
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-02-22 19:44:15 +0530

    Update nude.py

--- DAXXMUSIC/plugins/Yumi/nude.py
+++ DAXXMUSIC/plugins/Yumi/nude.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import filters
 from pyrogram.types import Message,InlineKeyboardButton,InlineKeyboardMarkup
 from pyrogram.enums import *
-from AvishaRobot import app
+from MukeshRobot import app
 
 
 @app.on_message(filters.command("nude"))
