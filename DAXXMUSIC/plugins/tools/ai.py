709309702458595ea99f37bd73a53d085bb7bef6
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-02-22 20:10:54 +0530

    Update ai.py

--- DAXXMUSIC/plugins/tools/ai.py
+++ DAXXMUSIC/plugins/tools/ai.py
@@ -2,7 +2,7 @@ import random
 import time
 import requests
 from DAXXMUSIC import app
-
+from config import BOT_USERNAME
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
