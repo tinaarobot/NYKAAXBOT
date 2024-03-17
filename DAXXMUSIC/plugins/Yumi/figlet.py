5d1c1e58908f68367aab93d88c4efa6d6a8158e5
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-17 14:14:46 +0530

    Update figlet.py

--- DAXXMUSIC/plugins/Yumi/figlet.py
+++ DAXXMUSIC/plugins/Yumi/figlet.py
@@ -18,18 +18,18 @@ async def echo(bot, message):
     try:
         text = message.text.split(' ',1)[1]
     except IndexError:
-        return await message.reply_text("Example:\n\n`/figlet DAXX PAPA `")
+        return await message.reply_text("๏ ᴇxᴀᴍᴘʟᴇ ➠ `/figlet ROY `")
     kul_text, keyboard = figle(text)
-    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ :\n<pre>{kul_text}</pre>", quote=True, reply_markup=keyboard)
+    await message.reply_text(f"๏ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ ➠\n<pre>{kul_text}</pre>", quote=True, reply_markup=keyboard)
 
 @app.on_callback_query(filters.regex("figlet"))
 async def figlet_handler(Client, query: CallbackQuery):
   try:
       kul_text, keyboard = figle(text)
-      await query.message.edit_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ :\n<pre>{kul_text}</pre>", reply_markup=keyboard)
+      await query.message.edit_text(f"๏ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ ➠\n<pre>{kul_text}</pre>", reply_markup=keyboard)
   except Exception as e : 
       await message.reply(e)
-__mod_name__ = "Fɪɢʟᴇᴛ" 
+__mod_name__ = "ғɪɢʟᴇᴛ" 
 __help__="""
-❍ /figlet*:* ᴍᴀᴋᴇs ғɪɢʟᴇᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
-Example:\n\n`/figlet DAXX PAPA `"""
+❍ /figlet *➠* ᴍᴀᴋᴇs ғɪɢʟᴇᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
+"""
