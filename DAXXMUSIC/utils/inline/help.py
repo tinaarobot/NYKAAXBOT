224d73d95283bff620f8dad6feb29f06fee5ca35
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-03-18 00:57:59 +0530

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -8,8 +8,12 @@ from DAXXMUSIC import app
 def help_pannel(_, START: Union[bool, int] = None):
     first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
     second = [
+InlineKeyboardButton(
+text=_["BACK_BUTTON"]
+callback_data="settingsback_helper",
+),
         InlineKeyboardButton(
-            text=_["BACK_BUTTON"],
+            text=_["CLOSE_BUTTON"],
             callback_data=f"close",
         ),
     ]
