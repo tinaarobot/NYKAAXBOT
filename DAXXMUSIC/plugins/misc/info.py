95252f8d6bc499a409e76b21976907f6c3e6ddd9
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-03-17 23:16:31 +0530

    Update info.py

--- DAXXMUSIC/plugins/misc/info.py
+++ DAXXMUSIC/plugins/misc/info.py
@@ -40,8 +40,8 @@ async def get_userinfo_img(
 
         circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
         circular_img.paste(img, (0, 0), mask)
-        resized = circular_img.resize((530, 530))
-        bg.paste(resized, (610, 90), resized)
+        resized = circular_img.resize((534, 534))
+        bg.paste(resized, (610, 89), resized)
 
     img_draw = ImageDraw.Draw(bg)
 
