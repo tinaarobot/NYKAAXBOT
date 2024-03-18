806311c7bf80d1563c45fa8530dc4d6b555307c2
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-03-18 23:44:27 +0530

    Update Welcome.py

--- DAXXMUSIC/plugins/tools/Welcome.py
+++ DAXXMUSIC/plugins/tools/Welcome.py
@@ -43,10 +43,10 @@ def welcomepic(pic, user, chat, id, uname):
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=50)
     font2 = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=90)
-    draw.text((70, 250), f'NAME: {unidecode(user)}', fill=(255, 255, 255), font=font)
-    draw.text((70, 340), f'ID: {id}', fill=(255, 255, 255), font=font)
-    draw.text((70, 430), f"USERNAME : {uname}", fill=(255,255,255),font=font)
-    pfp_position = (765, 129)  
+    draw.text((65, 250), f'NAME: {unidecode(user)}', fill=(255, 255, 255), font=font)
+    draw.text((65, 340), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((65, 430), f"USERNAME : {uname}", fill=(255,255,255),font=font)
+    pfp_position = (762, 129)  
     background.paste(pfp, pfp_position, pfp)  
     background.save(
         f"downloads/welcome#{id}.png"
