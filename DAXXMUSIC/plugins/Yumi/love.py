df78bf89d896706bfbe335f17240e655365e54d1
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 12:19:46 +0530

    Update love.py

--- DAXXMUSIC/plugins/Yumi/love.py
+++ DAXXMUSIC/plugins/Yumi/love.py
@@ -1,33 +1,9 @@
 from pyrogram import Client, filters
 import random
 from DAXXMUSIC import app
-from pyorogram.types import NIMI
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-
-NIMI = [
-    "https://telegra.ph/file/81918529691fc0f74f5c1.jpg",
-    "https://telegra.ph/file/d4b9114c757c1106c7432.jpg",
-    "https://telegra.ph/file/8486107891675afb52233.jpg",
-    "https://telegra.ph/file/bb37b6698a7b87dab1d86.jpg",
-    "https://telegra.ph/file/bb37b6698a7b87dab1d86.jpg",
-    "https://graph.org/file/942223799764b95b6028a.jpg",
-    "https://telegra.ph/file/592324136be542c2c9363.jpg",
-    "https://graph.org/file/ecd6670f7c9bde85b7378.jpg",
-    "https://telegra.ph/file/209d55403838bfd6498a5.jpg",
-    "https://telegra.ph/file/63004f4d786ac26da3a99.jpg",
-    "https://telegra.ph/file/f4791391e4e70e89a0640.jpg",
-    "https://telegra.ph/file/e5deebed1ae6b627818a3.jpg",
-    "https://telegra.ph/file/b66a6b8895f70f9643319.jpg",
-    "https://telegra.ph/file/c7818276d9c25b175d60e.jpg",
-    "https://telegra.ph/file/bf7707c8b3c2db8243948.jpg",
-    "https://telegra.ph/file/72e95777972186c14d978.jpg",
-    "https://graph.org/file/5c57cc6fff4ea7190e855.jpg",
-    "https://graph.org/file/6c2d0467bcafb7752d112.jpg",
-    "https://telegra.ph/file/7d52113479ecc4450817a.jpg",
-]
         
-
 def get_random_message(love_percentage):
     if love_percentage <= 30:
         return random.choice([
@@ -65,7 +41,8 @@ def love_command(client, message):
         love_message = get_random_message(love_percentage)
 
         response = f"❅ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟᴏᴠᴇ ᴘᴇʀᴄᴇɴᴛᴀɢᴇ ⏤͟͟͞͞★ \n\n❅ {name1} ♥️ + {name2} ♥️ = {love_percentage}%\n\n{love_message}"
+        client.send_message(message.chat.id, response, reply_markup=InlineKeyboardMarkup(EVAA),)
     else:
         response = "✦ ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛᴡᴏ ɴᴀᴍᴇs ᴀғᴛᴇʀ /lov ᴄᴏᴍᴍᴀɴᴅ."
-    app.send_photo(photo=random.choice(NIMI), response, reply_markup=InlineKeyboardMarkup(EVAA),)
+    client.send_message(message.chat.id, response, reply_markup=InlineKeyboardMarkup(EVAA),)
 
