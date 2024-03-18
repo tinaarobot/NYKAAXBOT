0373aeaf962e3c07357afb1a1b337f6645b5d3b8
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 22:05:16 +0530

    Update song.py

--- DAXXMUSIC/plugins/tools/song.py
+++ DAXXMUSIC/plugins/tools/song.py
@@ -61,7 +61,7 @@ def download_song(_, message):
             audio_file,
             thumb=thumb_name,
             title=title,
-            caption=f"✦ {title}\n\n ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➠{message.from_user.mention}\n ᴠɪᴇᴡs ➠ {views}\n ᴄʜᴀɴɴᴇʟ ➠ {channel_name}",
+            caption=f"✦ {title}\n\n❅ ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➠{message.from_user.mention}\n❅ ᴠɪᴇᴡs ➠ {views}\n❅ ᴄʜᴀɴɴᴇʟ ➠ {channel_name}",
             duration=dur
         )
         m.delete()
