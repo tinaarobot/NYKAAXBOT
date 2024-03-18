46ba74e398247853eda45895de79382964056e73
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 15:01:17 +0530

    Update pypi.py

--- DAXXMUSIC/plugins/Yumi/pypi.py
+++ DAXXMUSIC/plugins/Yumi/pypi.py
@@ -18,7 +18,7 @@ def get_pypi_info(package_name):
         return pypi_info
     
     except Exception as e:
-        print(f"Error fetching PyPI information: {e}")
+        print(f"✦ Error fetching PyPI information ➠ {e}")
         return None
 
 @app.on_message(filters.command("pypi", prefixes="/"))
@@ -32,19 +32,19 @@ def pypi_info_command(client, message):
         
         if pypi_info:
             # Creating a message with PyPI information
-            info_message = f"๏ ᴘᴀᴄᴋᴀɢᴇ ɴᴀᴍᴇ ➛ {pypi_info['info']['name']}\n\n" \
-                           f"๏ ʟᴀᴛᴇsᴛ ᴠᴇʀsɪᴏɴ ➛ {pypi_info['info']['version']}\n\n" \
-                           f"๏ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➛ {pypi_info['info']['summary']}\n\n" \
-                           f"๏ ᴘʀᴏᴊᴇᴄᴛ ᴜʀʟ ➛ {pypi_info['info']['project_urls']['Homepage']}"
+            info_message = f"๏ ᴘᴀᴄᴋᴀɢᴇ ɴᴀᴍᴇ ➠ {pypi_info['info']['name']}\n\n" \
+                           f"๏ ʟᴀᴛᴇsᴛ ᴠᴇʀsɪᴏɴ ➠ {pypi_info['info']['version']}\n\n" \
+                           f"๏ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➠ {pypi_info['info']['summary']}\n\n" \
+                           f"๏ ᴘʀᴏᴊᴇᴄᴛ ᴜʀʟ ➠ {pypi_info['info']['project_urls']['Homepage']}"
             
             # Sending the PyPI information back to the user
             client.send_message(message.chat.id, info_message)
         
         else:
             # Handling the case where information retrieval failed
-            client.send_message(message.chat.id, "Failed to fetch information from PyPI.")
+            client.send_message(message.chat.id, "✦ Failed to fetch information from PyPI.")
     
     except IndexError:
 
-        client.send_message(message.chat.id, "Please provide a package name after the /pypi command.")
+        client.send_message(message.chat.id, "✦ Please provide a package name after the /pypi command.")
 
