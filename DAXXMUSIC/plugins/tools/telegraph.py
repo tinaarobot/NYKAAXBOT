321ad956eb1a8ed74ca85b332c9b49b154a941e8
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 22:12:23 +0530

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -4,7 +4,7 @@ from DAXXMUSIC import app
 from pyrogram.types import InputMediaPhoto
 
 
-@app.on_message(filters.command(["tgm" , "link"]))
+@app.on_message(filters.command(["tgm" , "tm"]))
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
