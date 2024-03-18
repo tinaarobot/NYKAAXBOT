0b4f98f1d7306d08408caaf8a00374f6cca71330
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 20:54:46 +0530

    Update dj.py

--- DAXXMUSIC/plugins/play/dj.py
+++ DAXXMUSIC/plugins/play/dj.py
@@ -39,7 +39,7 @@ def apply_bass_boost(audio_path):
     boosted_audio = audio.low_pass_filter(200).high_pass_filter(70).apply_gain(30)
 
     # Save the boosted audio as a temporary file
-    boosted_audio_path = "yumiboost.mp3"
+    boosted_audio_path = "nykaaboost.mp3"
     boosted_audio.export(boosted_audio_path, format="mp3")
 
     return boosted_audio_path
