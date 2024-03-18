994b19474aa2a8b2c10713ba81a04a5c33477ada
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 20:52:42 +0530

    Update urlshortner.py

--- DAXXMUSIC/plugins/misc/urlshortner.py
+++ DAXXMUSIC/plugins/misc/urlshortner.py
@@ -15,7 +15,7 @@ async def short_urls(bot, message):
     await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Example:**\n\n`/short [url]`")
+            "**✦ ᴇxᴀᴍᴘʟᴇ ➠** `/short [url]`")
 #     url_pattern = re.compile(r'https?://\S+')
     link=message.command[1]
 #     link = url_pattern.findall(urls)
@@ -35,24 +35,24 @@ async def short_urls(bot, message):
 
         shorted=[tiny_link,dagd_link,clckru_link]
         url=[
-        [ikb("Tiny Url",url=tiny_link)],
+        [ikb("ᴛɪɴʏ ᴜʀʟ",url=tiny_link)],
 
-        [ikb("Dagd Url",url=dagd_link),
+        [ikb("ᴅᴀɢᴅ ᴜʀʟ",url=dagd_link),
 
-         ikb("Clckru Url",url=clckru_link)
+         ikb("ᴄʟᴄᴋʀᴜ ᴜʀʟ",url=clckru_link)
         ]
         ]
-        await message.reply_text(f"Here are few shortened links :",reply_markup=ikm(url))
+        await message.reply_text(f"✦ ʜᴇʀᴇ ᴀʀᴇ ғᴇᴡ sʜᴏʀᴛᴇᴅ ᴜʀʟ ʟɪɴᴋs ⏤͟͟͞͞★",reply_markup=ikm(url))
 
     except Exception as e:
-        await message.reply_text(f"Either the link is already shortened or is invalid.")
+        await message.reply_text(f"✦ Either the link is already shortened or is invalid.")
 
 @app.on_message(filters.command(["unshort"]))
 async def unshort(bot, message):
     await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Example:**\n\n`/unshort [short - url]`")
+            "**✦ ᴇxᴀᴍᴘʟᴇ **➠`/unshort [short - url]`")
     link=message.text.split(' ')[1]
     
     try:
@@ -63,7 +63,7 @@ async def unshort(bot, message):
 
         [ikb
 
-         ("View Link",url=x)
+         ("ᴠɪᴇᴡ ʟɪɴᴋ",url=x)
 
         ]
 
@@ -71,13 +71,13 @@ async def unshort(bot, message):
 
         
 
-        await message.reply_text(f"Here's the unshortened link :\n`{x}` " ,reply_markup=ikm(url))
+        await message.reply_text(f"✦ ʜᴇʀᴇ's ᴛʜᴇ ᴜɴsʜᴏʀᴛᴇᴅ ᴜʀʟ ʟɪɴᴋ ⏤͟͟͞͞★\n\n♥︎ `{x}` " ,reply_markup=ikm(url))
 
         
 
     except Exception as e:
 
-        await message.reply_text(f"ᴇʀʀᴏʀ:    {e} ")
+        await message.reply_text(f"✦ ᴇʀʀᴏʀ ➠ {e} ")
 # app.add_handler(MessageHandler(short_urls))
 # app.add_handler(MessageHandler(unshort))
 __help__ = """
