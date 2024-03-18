dad19447113725355404f0fff303df55d752bc60
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 13:06:02 +0530

    Update hexacode.py

--- DAXXMUSIC/plugins/Yumi/hexacode.py
+++ DAXXMUSIC/plugins/Yumi/hexacode.py
@@ -26,7 +26,7 @@ def convert_text(_, message):
         hex_representation = text_to_hex(input_text)
         decoded_text = hex_to_text(input_text)
 
-        response_text = f"𝗜𝗻𝗽𝘂𝘁 𝗧𝗲𝘅𝘁➪\n {input_text}\n\n𝗛𝗲𝘅 𝗥𝗲𝗽𝗿𝗲𝘀𝗲𝗻𝘁𝗮𝘁𝗶𝗼𝗻➪\n {hex_representation}\n\n𝗗𝗲𝗰𝗼𝗱𝗲𝗱 𝗧𝗲𝘅𝘁➪\n {decoded_text}\n\n\n𝗕𝗬 ➪@{BOT_USERNAME}"
+        response_text = f"๏ ɪɴᴘᴜᴛ ᴛᴇxᴛ ➠\n {input_text}\n\n๏ ʜᴇx ʀᴇᴘʀᴇsᴇɴᴛᴀᴛɪᴏɴ ➠\n {hex_representation}\n\n๏ ᴅᴇᴄᴏᴅᴇᴅ ᴛᴇxᴛ ➠\n {decoded_text}\n\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐"
 
         message.reply_text(response_text)
     else:
