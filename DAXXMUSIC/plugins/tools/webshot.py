ad31391e977bbd3b39028cd60c2d83f7db5fa7a7
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-18 22:22:22 +0530

    Update webshot.py

--- DAXXMUSIC/plugins/tools/webshot.py
+++ DAXXMUSIC/plugins/tools/webshot.py
@@ -8,7 +8,7 @@ from pyrogram.types import *
 from DAXXMUSIC import app
 
 button = InlineKeyboardMarkup([[
-            InlineKeyboardButton("⌯ ᴄʟᴏsᴇ ⌯", callback_data="close_data")
+            InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close_data")
                               ]])
 
 
@@ -62,7 +62,7 @@ async def eor(msg: Message, **kwargs):
 @app.on_message(filters.command(["webss", "ss", "webshot"]))
 async def take_ss(_, message: Message):
     if len(message.command) < 2:
-        return await eor(message, text="**ɢɪᴠᴇ ᴀ ᴜʀʟ ᴛᴏ ғᴇᴛᴄʜ sᴄʀᴇᴇɴsʜᴏᴛ.**")
+        return await eor(message, text="**✦ ɢɪᴠᴇ ᴀ ᴜʀʟ ᴛᴏ ғᴇᴛᴄʜ sᴄʀᴇᴇɴsʜᴏᴛ.**")
 
     if len(message.command) == 2:
         url = message.text.split(None, 1)[1]
@@ -76,16 +76,16 @@ async def take_ss(_, message: Message):
             "true",
         ]
     else:
-        return await eor(message, text="**ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ.**")
+        return await eor(message, text="**✦ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ.**")
 
-    m = await eor(message, text="**ᴄᴀᴘᴛᴜʀɪɴɢ sᴄʀᴇᴇɴsʜᴏᴛ...**")
+    m = await eor(message, text="**✦ ᴄᴀᴘᴛᴜʀɪɴɢ sᴄʀᴇᴇɴsʜᴏᴛ...**")
 
     try:
         photo = await take_screenshot(url, full)
         if not photo:
-            return await m.edit("**ғᴀɪʟᴇᴅ ᴛᴏ ᴛᴀᴋᴇ sᴄʀᴇᴇɴsʜᴏᴛ.**")
+            return await m.edit("**✦ ғᴀɪʟᴇᴅ ᴛᴏ ᴛᴀᴋᴇ sᴄʀᴇᴇɴsʜᴏᴛ.**")
 
-        m = await m.edit("**ᴜᴘʟᴏᴀᴅɪɴɢ...**")
+        m = await m.edit("**✦ ᴜᴘʟᴏᴀᴅɪɴɢ...**")
 
         if not full:
             await message.reply_photo(photo, reply_markup=button)
