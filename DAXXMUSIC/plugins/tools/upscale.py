0e018068d664496fb1c84eed11977e8abe998ae0
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-23 19:07:53 +0530

    Update upscale.py

--- DAXXMUSIC/plugins/tools/upscale.py
+++ DAXXMUSIC/plugins/tools/upscale.py
@@ -11,7 +11,7 @@ from uuid import uuid4
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 
 
-@app.on_message(filters.reply & filters.command("upscale"))
+@app.on_message(filters.reply & filters.command("upsc"))
 async def upscale_image(app, message):
     try:
         if not message.reply_to_message or not message.reply_to_message.photo:
@@ -45,38 +45,3 @@ async def upscale_image(app, message):
         await message.reply_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ**.")
 
 
-# ------------
-
-
-waifu_api_url = 'https://api.waifu.im/search'
-
-# IAM_DAXX
-
-def get_waifu_data(tags):
-    params = {
-        'included_tags': tags,
-        'height': '>=2000'
-    }
-
-    response = requests.get(waifu_api_url, params=params)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-@app.on_message(filters.command("waifu"))
-def waifu_command(client, message):
-    try:
-        tags = ['maid']  # You can customize the tags as needed
-        waifu_data = get_waifu_data(tags)
-
-        if waifu_data and 'images' in waifu_data:
-            first_image = waifu_data['images'][0]
-            image_url = first_image['url']
-            message.reply_photo(image_url)
-        else:
-            message.reply_text("No waifu found with the specified tags.")
-
-    except Exception as e:
-        message.reply_text(f"An error occurred: {str(e)}")
