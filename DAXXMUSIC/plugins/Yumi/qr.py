fceb570662dac97d37d45f122cd0541161bac041
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-24 14:34:33 +0530

    Update qr.py

--- DAXXMUSIC/plugins/Yumi/qr.py
+++ DAXXMUSIC/plugins/Yumi/qr.py
@@ -42,7 +42,7 @@ def qr_handler(client, message: Message):
     if len(command_text) > 1:
         input_text = " ".join(command_text[1:])
         qr_image = generate_qr_code(input_text)
-        message.reply_photo(qr_image, caption="✦ ǫʀ sᴜᴄᴄᴇssғᴜʟʟʏ ɢᴇɴʀsᴛᴇᴅ ✦\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐ ", reply_markup=InlineKeyboardMarkup(EVAA),)
+        message.reply_photo(qr_image, caption="✦ ǫʀ ᴄᴏᴅᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ɢᴇɴʀsᴛᴇᴅ ✦\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐ ", reply_markup=InlineKeyboardMarkup(EVAA),)
 
     else:
         message.reply_text("✦ Please provide the text for the QR code after the command. Example usage ➠ /qr text")
