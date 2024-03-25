20fc269017a4135ec78432babbd8af4c83077070
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-25 10:32:10 +0530

    Create decode.py

---
+++ DAXXMUSIC/plugins/tools/decode.py
@@ -0,0 +1,76 @@
+
+import requests
+from DAXXMUSIC import app
+from config import BOT_NAME,BOT_USERNAME
+import time
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
+
+@app.on_message(filters.command(["morseencode"]))
+async def morse_en(bot, message):
+    
+    try:
+        
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "❍ ᴇxᴀᴍᴘʟᴇ ➛ /morseencode <ǫᴜᴇʀʏ>")
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://mukesh-api.vercel.app/morse/encode/{a}') 
+            x=response.json()["results"]
+            
+            await message.reply_text(f"{x}", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**❍ ᴇʀʀᴏʀ ➛ {e} ")
+@app.on_message(filters.command("morsedecode"))
+async def morse_de(bot, message):
+    
+    try:
+        
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "❍ ᴇxᴀᴍᴘʟᴇ ➛ /morsedecode <ǫᴜᴇʀʏ>")
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://mukesh-api.vercel.app/morse/decode/{a}') 
+            x=response.json()["results"]
+            
+            await message.reply_text(f"{x}", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**❍ ᴇʀʀᴏʀ ➛ {e} ")
+@app.on_message(filters.command(["encode"]))
+async def base_en(bot, message):
+    
+    try:
+        
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "❍ ᴇxᴀᴍᴘʟᴇ ➛ /encode <query>")
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://mukesh-api.vercel.app/base/encode/{a}') 
+            x=response.json()["results"]
+            
+            await message.reply_text(f" {x}", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**❍ ᴇʀʀᴏʀ ➛ {e} ")
+@app.on_message(filters.command(["decode"]))
+async def base_de(bot, message):
+    
+    try:
+        
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "❍ ᴇxᴀᴍᴘʟᴇ ➛ /decode <ǫᴜᴇʀʏ>")
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://mukesh-api.vercel.app/base/decode/{a}') 
+            x=response.json()["results"]
+            
+            await message.reply_text(f" {x}", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**❍ ᴇʀʀᴏʀ ➛ {e} ")
