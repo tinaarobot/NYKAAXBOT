b1f9354f0ebc1ba5301027ec91771d39149f6723
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-03-25 10:30:16 +0530

    Create mahadev.py

---
+++ DAXXMUSIC/plugins/tools/mahadev.py
@@ -0,0 +1,30 @@
+
+import requests
+from DAXXMUSIC import app
+from config import BOT_NAME, BOT_USERNAME
+import time
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
+
+@app.on_message(filters.command("mahadev"))
+async def Mahadev(bot, message):
+    try:
+        
+        await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO) 
+        response = requests.get(f'https://mukesh-api.vercel.app/mahadev') 
+        x=response.json()["results"]
+            
+        await message.reply_photo(photo=x,caption=f" \n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [๛ɴ ʏ ᴋ ᴀ ᴀ ༗](https://t.me/nykaaxbot) ", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
+@app.on_message(filters.command("uselessfact"))
+async def uselessa_fact(bot, message):
+    try:
+        
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING) 
+        response = requests.get(f'https://mukesh-api.vercel.app/uselessfact') 
+        x=response.json()["results"]
+            
+        await message.reply_text(x, parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
